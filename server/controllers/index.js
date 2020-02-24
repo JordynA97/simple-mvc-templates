58c30ba80192ba1425df944d15c98dc9f24bd7f6
JordynA97 <jla5483@rit.edu>
2020-02-24 14:36:59 -0500

    complete

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,27 +3,41 @@
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  res.render('index', {
+    title: 'Home.',
+    pageName: 'Homepage',
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
-
+  res.render('page1', {
+    title: 'Page 1',
+  });
 };
 
 const hostPage2 = (req, res) => {
-
+  res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname){
+    return res.status(400).json({error: 'Firstname and lastname are required'});
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  res.json({name});
 };
 
 const notFound = (req, res) => {
-
+  //set status to 404 and render the html page
+  res.status(404).render('notFound', {
+    page: req.url,
+  });
 };
 
 module.exports = {
